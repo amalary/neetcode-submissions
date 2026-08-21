62dae00581cb5136055b976d94eb4d5b4ae11d3e
Anthony Malary <75505082+amalary@users.noreply.github.com>
2026-08-20 20:46:09 -0700

    Add: max-consecutive-ones - submission-6

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int: 
+
+        # I am going to create a counter for the consecutive 1's in the array 
+        #Going to create a variable to keep track of the longeststreak of consecutive 1's 
+        #If the number in the array is equal to 1 the counter will increament if it is not itll be skipped over 
+        # Finally I'll return the value for the longeststreak for 1's 
+
+        counter = 0 
+
+        longeststreak = 0 
+
+        for num in nums: 
+
+            if num == 1: 
+
+                counter+= 1 
+
+                longeststreak = max(longeststreak,counter)
+
+            else: 
+
+                counter = 0 
+
+        return longeststreak 
